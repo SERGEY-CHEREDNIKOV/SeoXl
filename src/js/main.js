176e3339456aaaed347d844d69cdba2269f09ee5
Sergey <serezha.ros@mail.ru>
2021-10-19 17:32:48 +0300

    section abaut-us

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@
 
 import burger from "./modules/burger"; 
 import slider from "./modules/slider"; 
+import scrolling from "./modules/scrolling"; 
 
 window.addEventListener('load', function () {
     'use strict';
 
     burger();  
     slider();
+    scrolling();
 });
